45cb31e457730f338061acbd8d2dab71148df960
Yared <yareddej@gmail.com>
2020-06-09 00:23:58 +0200

    Initial commit

---
+++ 00_unzip/bash_scripts/process_job.sh
@@ -0,0 +1,36 @@
+#!/bin/bash 
+# arg1: work_path
+# arg2: code directory
+# arg3: input directory
+# arg4: work directory
+# arg5: output directory
+# arg6: mq_read
+# arg7: mq_write
+
+work_path=${1}
+code_directory=${2}
+input_directory=${3}
+work_directory=${4}
+output_directory=${5}
+mq_write=${7}
+
+
+echo '#'
+echo '#  Starting Process: Read zip files'
+echo '#'
+
+#import log util functions 
+. ${code_directory}/util.sh
+
+file_name=${work_path##*/}
+
+log_info "Started file processing: collect files from source directory" "${file_name}" "$0"
+
+# Move the files to output and write the new url to the message queue
+${code_directory}/move_to_output.sh "${code_directory}" "${mq_write}" "${output_directory}" "${work_path}" \
+    || handle_error "Error in collecting files from source directory" "${file_name}" "$0" "$LINENO"  
+
+echo '   Done'
+
+
+
