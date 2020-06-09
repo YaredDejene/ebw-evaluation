d92d013a6c3f3667005b3986d1b2af9a7abb4eb1
Yared <yareddej@gmail.com>
2020-06-09 21:32:43 +0200

    tsv2csv step added

--- 00_unzip/bash_scripts/process_job.sh
+++ 00_unzip/bash_scripts/process_job.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 # arg1: work_path
 # arg2: code directory
 # arg3: input directory
@@ -16,21 +16,38 @@ mq_write=${7}
 
 
 echo '#'
-echo '#  Starting Process: Read zip files'
+echo '#  Starting Process: Unzipping ', ${work_path}
 echo '#'
 
 #import log util functions 
 . ${code_directory}/util.sh
 
-file_name=${work_path##*/}
 
-log_info "Started file processing: collect files from source directory" "${file_name}" "$0"
+# Construct Paths
+file_name=${work_path##*/}
+file_name_no_ext=${file_name%.*}
+extract_directory=${work_directory}/${file_name_no_ext}
+
+
+# Debug: Show Paths
+echo "!! extract_directory", ${extract_directory}
+log_info "Started extracting into ${extract_directory}" "${file_name}" "$0"
+
+# Extract Zip
+echo "   Extracting without folder structure"
+unzip -j ${work_path} -d ${extract_directory} \
+    || handle_error "Error while extracting a file: ${file_name} into ${extract_directory} directory " "${file_name}" "$0" "$LINENO"  
+
+log_info "Done extracting without folder structure" "${file_name}" "$0"
 
 # Move the files to output and write the new url to the message queue
-${code_directory}/move_to_output.sh "${code_directory}" "${mq_write}" "${output_directory}" "${work_path}" \
-    || handle_error "Error in collecting files from source directory" "${file_name}" "$0" "$LINENO"  
+log_info "Started moving extracted files into output directory" "${file_name}" "$0"
+${code_directory}/move_to_output.sh ${code_directory} ${mq_write} ${output_directory} ${extract_directory}/*  \
+    || handle_error "Error occured while moving extracted files into output directory" "${file_name}" "$0" "$LINENO"
+
+# Cleanup
+rm -rf $extract_directory
+log_info "Removed temporary working directory: ${extract_directory}" ${file_name} "$0"
 
 echo '   Done'
-
-
 
